439744f3c930ab51d1438d31f436e355df18eb4d
Gray Pham <hunghtk456@gmail.com>
2022-06-04 22:56:25 +0700

    Đổi tên và fixbug

--- DAL/PostModel.Designer.cs
+++ DAL/PostModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\hungh\Desktop\OakCourse\DAL\PostModel.edmx'. 
+﻿// T4 code generation is enabled for model 'F:\ASP.net\Đồ án IT project\DAL\PostModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
